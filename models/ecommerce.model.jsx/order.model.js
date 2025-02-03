51bac69c0992af86045ac5cc9e7af2b4408704d8
Ikhlas Gadagin <ikhlashgadagin@gmail.com>
2025-02-03 14:37:44 +0530

    order model is added

--- models/ecommerce.model.jsx/order.model.js
+++ models/ecommerce.model.jsx/order.model.js
@@ -1,5 +1,32 @@
 import mongoose from "mongoose";
 
-const orderSchema = new mongoose.Schema({}, { timestamps: true });
+const orderItemSchema = new mongoose.Schema({
+    productId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product"
+    },
+    quantity: Number,
+    price: Number
+});
+
+
+const orderSchema = new mongoose.Schema({
+    orderItems:{
+        type: [orderItemSchema],
+    },
+    paymentIntent: {},
+    orderStatus: {
+        type: String,
+        default: "Not Processed",
+        enum: [
+            "Not Processed",
+            "Processing",
+            "Dispatched",
+            "Cancelled",
+            "Completed"
+        ]
+    },
+    orderedBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" }
+}, { timestamps: true });
 
 export const Order = mongoose.model("Order", orderSchema);
